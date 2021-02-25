1594f50ff959f2c6d14c4d8d2f1425153404af09
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-02-24 19:24:25 -0500

    added some CSS decorations and started declaring JavaScript variables.

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Assigning variables to HTML elements
+var startButton = document.getElementById
+var answers;
+var highScore = 0;
+var timer;
+// push start button to begin game
+
+//questions 1 appears
+// timer starts
+// 
